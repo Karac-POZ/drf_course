157ae24703d91ac79edf7a7581ef5554111afdf7
Rasul Shaidullinov <rasul200601@gmail.com>
2023-12-10 22:28:15 +0600

    1 day

---
+++ backend/core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django_extensions.db.models import (
+    ActivatorModel,
+    TimeStampedModel,
+    TitleDescriptionModel,
+)
+from utils.model_abstracts import Model
+
+
+class Contact(TimeStampedModel, ActivatorModel, TitleDescriptionModel, Model):
+    class Meta:
+        verbose_name_plural = "Contacts"
+
+    email = models.EmailField(verbose_name="Email")
+
+    def __str__(self):
+        return f"{self.title}"
